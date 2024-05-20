e62563b09e5826b6504ef4a36c462ea7c718ef52
yuta adachi <info@rcwas.com>
2024-05-20 14:04:30 +0900

    5/11課題：キャラクターの表示・非表示を切り替えて歩かせる。

--- sample_20240511/challenge/script.js
+++ sample_20240511/challenge/script.js
@@ -15,6 +15,10 @@ window.addEventListener('DOMContentLoaded', () => {
   app.render();
 }, false);
 
+// キャラクター表示・非表示切り替えのフラグ
+let character01_flag = true;
+
+
 class ThreeApp {
   /**
    * カメラ定義のための定数
@@ -27,9 +31,9 @@ class ThreeApp {
     // 描画する空間のニアクリップ面（最近面）
     near: 0.1,
     // 描画する空間のファークリップ面（最遠面）
-    far: 50.0,
+    far: 200.0,
     // カメラの座標
-    position: new THREE.Vector3(0.0, 2.0, 30.0),
+    position: new THREE.Vector3(0.0, 2.0, 100.0),
     // カメラの注視点
     lookAt: new THREE.Vector3(0.0, 0.0, 0.0),
   };
@@ -189,7 +193,7 @@ class ThreeApp {
     //   this.torusArray.push(torus);
     // }
     const boxCount_01 = 308;
-    const boxCount_02 = 308;
+    const boxCount_02 = 292;
     // const transformScale = 5.0;
     this.boxGeometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
     this.characterArray01 = [];
@@ -391,8 +395,8 @@ class ThreeApp {
         default:
           break;
       }
-      // this.scene.add(character01);
-      // this.characterArray01.push(character01);
+      this.scene.add(character01);
+      this.characterArray01.push(character01);
       // if (i < 68) {
       // } else {
       //   // character01.position.z = -1;
@@ -407,55 +411,55 @@ class ThreeApp {
       // 色の指定
       switch (i) {
         // 黒：212121
-        case 0: case 5: case 6: case 7: case 8: case 15: case 16: case 26: case 27: case 28: case 38: case 39: case 50: case 52:
+        case 0: case 5: case 6: case 7: case 8: case 15: case 16: case 26: case 27: case 28: case 38: case 39: case 50: case 52: case 53: case 64: case 67: case 68: case 79: case 82: case 83: case 94: case 97: case 109: case 112: case 124: case 127: case 140: case 142: case 145: case 156: case 160: case 169: case 181: case 187: case 190: case 220: case 221: case 233: case 235: case 238: case 240: case 246: case 247: case 248: case 251: case 252: case 253: case 255: case 256: case 259: case 260: case 261: case 264: case 265: case 268: case 269: case 272: case 273: case 276: case 277: case 278: case 279: case 280: case 281: case 282: case 283: case 285: case 286: case 287: case 288: case 289: case 290: case 291:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_01);
           break;
         // 濃紺：313151
-        case 20: case 42:
+        case 20: case 42: case 58: case 98: case 103: case 104: case 113: case 117: case 122: case 128: case 130: case 131: case 138: case 143: case 154: case 157: case 159: case 170: case 172: case 182: case 184: case 195: case 197: case 208: case 210: case 219: case 222: case 226: case 231: case 236: case 249: case 262: case 274: case 284:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_02);
           break;
         // グレー：D1D9D9
-        case 11: case 12: case 23: case 24:
+        case 11: case 12: case 23: case 24: case 69: case 74: case 75: case 84: case 85: case 119: case 125: case 129: case 144: case 158: case 163: case 171: case 183: case 196: case 198: case 209: case 211: case 212: case 213: case 223: case 227: case 230: case 237: case 242: case 243: case 250: case 263:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_03);
           break;
         // 首元のブルー：5939A9
-        // case 144:
-        //   character02 = new THREE.Mesh(this.boxGeometry, this.material_04);
-        //   break;
+        case 133: case 136: case 147: case 152: case 167: case 168:
+          character02 = new THREE.Mesh(this.boxGeometry, this.material_04);
+          break;
         // 濃いグレー：514951
-        // case 239:
-        //   character02 = new THREE.Mesh(this.boxGeometry, this.material_05);
-        //   break;
+        case 224: case 232: case 239: case 244: case 257: case 266: case 270: case 271:
+          character02 = new THREE.Mesh(this.boxGeometry, this.material_05);
+          break;
         // むらさき：8971D9
-        // case 128:
-        //   character02 = new THREE.Mesh(this.boxGeometry, this.material_06);
-        //   break;
+        case 114: case 118: case 120: case 121: case 132: case 137: case 146: case 153: case 225: case 234: case 241: case 245: case 254: case 258: case 267: case 275:
+          character02 = new THREE.Mesh(this.boxGeometry, this.material_06);
+          break;
         // 額当てのブルー：194999
-        case 30: case 31: case 32: case 37: case 44: case 48: case 49:
+        case 30: case 31: case 32: case 37: case 44: case 48: case 49: case 62: case 63: case 89: case 90: case 93: case 107: case 214: case 215: case 228: case 229:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_07);
           break;
         // きいろ：E9B921
-        case 10: case 22: case 25: case 34: case 35: case 40: case 41:
+        case 10: case 22: case 25: case 34: case 35: case 40: case 41: case 54: case 55: case 70: case 101: case 115: case 205: case 206: case 216: case 217: case 218:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_08);
           break;
         // ちゃいろ：612121
-        case 1: case 9:
+        case 1: case 9: case 56: case 71: case 81: case 86: case 87: case 96: case 99: case 111: case 134: case 135: case 175: case 178: case 185: case 192: case 194: case 199: case 201: case 204: case 207:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_09);
           break;
         // 肌色：E9A999
-        case 51:
+        case 51: case 57: case 65: case 72: case 77: case 78: case 80: case 91: case 92: case 95: case 105: case 106: case 110: case 141: case 148: case 149: case 150: case 151: case 161: case 162: case 164: case 165: case 166: case 174: case 176: case 177: case 179: case 186: case 188: case 189: case 191: case 200: case 202: case 203:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_10);
           break;
         // オレンジ：D16101
-        case 2: case 3: case 4: case 13: case 14: case 17: case 18: case 19: case 21: case 33: case 36:
+        case 2: case 3: case 4: case 13: case 14: case 17: case 18: case 19: case 21: case 33: case 36: case 100: case 116: case 193:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_11);
           break;
         // 薄い茶色：B97959
-        // case 56:
-        //   character02 = new THREE.Mesh(this.boxGeometry, this.material_12);
-        //   break;
+        case 66: case 73: case 76: case 126: case 139: case 173: case 180:
+          character02 = new THREE.Mesh(this.boxGeometry, this.material_12);
+          break;
         // ブルー：2179D9
-        case 29: case 43: case 45: case 46: case 47:
+        case 29: case 43: case 45: case 46: case 47: case 59: case 60: case 61: case 88: case 102: case 108: case 123: case 155:
           character02 = new THREE.Mesh(this.boxGeometry, this.material_13);
           break;
         default:
@@ -478,63 +482,117 @@ class ThreeApp {
         case 39: case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: case 50: case 51: case 52:
           character02.position.y = -7;
           break;
+        case 53: case 54: case 55: case 56: case 57: case 58: case 59: case 60: case 61: case 62: case 63: case 64: case 65: case 66: case 67:
+          character02.position.y = -6;
+          break;
+        case 68: case 69: case 70: case 71: case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 79: case 80: case 81: case 82:
+          character02.position.y = -5;
+          break;
+        case 83: case 84: case 85: case 86: case 87: case 88: case 89: case 90: case 91: case 92: case 93: case 94: case 95: case 96: case 97:
+          character02.position.y = -4;
+          break;
+        case 98: case 99: case 100: case 101: case 102: case 103: case 104: case 105: case 106: case 107: case 108: case 109: case 110: case 111: case 112:
+          character02.position.y = -3;
+          break;
+        case 113: case 114: case 115: case 116: case 117: case 118: case 119: case 120: case 121: case 122: case 123: case 124: case 125: case 126: case 127:
+          character02.position.y = -2;
+          break;
+        case 128: case 129: case 130: case 131: case 132: case 133: case 134: case 135: case 136: case 137: case 138: case 139: case 140: case 141: case 142:
+          character02.position.y = -1;
+          break;
+        case 143: case 144: case 145: case 146: case 147: case 148: case 149: case 150: case 151: case 152: case 153: case 154: case 155: case 156:
+          character02.position.y = 0;
+          break;
+        case 157: case 158: case 159: case 160: case 161: case 162: case 163: case 164: case 165: case 166: case 167: case 168: case 169:
+          character02.position.y = 1;
+          break;
+        case 170: case 171: case 172: case 173: case 174: case 175: case 176: case 177: case 178: case 179: case 180: case 181:
+          character02.position.y = 2;
+          break;
+        case 182: case 183: case 184: case 185: case 186: case 187: case 188: case 189: case 190: case 191: case 192: case 193: case 194:
+          character02.position.y = 3;
+          break;
+        case 195: case 196: case 197: case 198: case 199: case 200: case 201: case 202: case 203: case 204: case 205: case 206: case 207:
+          character02.position.y = 4;
+          break;
+        case 208: case 209: case 210: case 211: case 212: case 213: case 214: case 215: case 216: case 217: case 218: case 219: case 220: case 221:
+          character02.position.y = 5;
+          break;
+        case 222: case 223: case 224: case 225: case 226: case 227: case 228: case 229: case 230: case 231: case 232: case 233: case 234: case 235:
+          character02.position.y = 6;
+          break;
+        case 236: case 237: case 238: case 239: case 240: case 241: case 242: case 243: case 244: case 245: case 246: case 247: case 248:
+          character02.position.y = 7;
+          break;
+        case 249: case 250: case 251: case 252: case 253: case 254: case 255: case 256: case 257: case 258: case 259: case 260: case 261:
+          character02.position.y = 8;
+          break;
+        case 262: case 263: case 264: case 265: case 266: case 267: case 268: case 269: case 270: case 271: case 272: case 273:
+          character02.position.y = 9;
+          break;
+        case 274: case 275: case 276: case 277: case 278: case 279: case 280: case 281: case 282: case 283:
+          character02.position.y = 10;
+          break;
+        case 284: case 285: case 286: case 287: case 288: case 289: case 290:
+          character02.position.y = 11;
+          break;
+        case 291:
+          character02.position.y = 12;
+          break;
         default:
           break;
       }
       // x軸の指定（8px目をセンターにする：-8 ~ 7）
       switch (i) {
-        // case 74: case 90: case 106: case 122: case 138: case 153: case 167: case 179: case 192: case 206: case 221: case 236: case 250:
-        //   character02.position.x = -8;
-        //   break;
-        // case 29: case 44: case 59: case 75: case 91: case 107: case 123: case 139: case 154: case 168: case 180: case 193: case 207: case 222: case 237: case 251: case 265: case 278:
-        //   character02.position.x = -7;
-        //   break;
-        case 39:
+        case 53: case 68: case 83: case 98: case 113: case 128: case 143: case 157: case 170: case 182: case 195: case 208: case 222: case 236: case 249: case 262: case 274:
+          character02.position.x = -7;
+          break;
+        case 39: case 54: case 69: case 84: case 99: case 114: case 129: case 144: case 158: case 171: case 183: case 196: case 209: case 223: case 237: case 250: case 263: case 275: case 284:
           character02.position.x = -6;
           break;
-        case 16: case 27: case 40:
+        case 16: case 27: case 40: case 55: case 70: case 85: case 100: case 115: case 130: case 145: case 159: case 172: case 184: case 197: case 210: case 224: case 238: case 251: case 264: case 276:
           character02.position.x = -5;
           break;
-        case 6: case 17: case 28: case 41:
+        case 6: case 17: case 28: case 41: case 56: case 71: case 86: case 101: case 116: case 131: case 146: case 160: case 173: case 185: case 198: case 211: case 225: case 239: case 252: case 285:
           character02.position.x = -4;
           break;
-        case 7: case 18: case 29: case 42:
+        case 7: case 18: case 29: case 42: case 57: case 72: case 87: case 102: case 117: case 132: case 147: case 161: case 174: case 186: case 199: case 212: case 226: case 240: case 253: case 265: case 277:
           character02.position.x = -3;
           break;
-        case 8: case 19: case 30: case 43:
+        case 8: case 19: case 30: case 43: case 58: case 73: case 88: case 103: case 118: case 133: case 148: case 162: case 175: case 187: case 200: case 213: case 227: case 241: case 254: case 266: case 278: case 286:
           character02.position.x = -2;
           break;
-        case 0: case 9: case 20: case 31: case 44:
+        case 0: case 9: case 20: case 31: case 44: case 59: case 74: case 89: case 104: case 119: case 134: case 149: case 163: case 176: case 188: case 201: case 214: case 228: case 242: case 255: case 267: case 279:
           character02.position.x = -1;
           break;
-        case 1: case 10: case 21: case 32: case 45:
+        case 1: case 10: case 21: case 32: case 45: case 60: case 75: case 90: case 105: case 120: case 135: case 150: case 164: case 177: case 189: case 202: case 215: case 229: case 243: case 256: case 268: case 280: case 287:
           character02.position.x = 0;
           break;
-        case 2: case 11: case 22: case 33: case 46:
+        case 2: case 11: case 22: case 33: case 46: case 61: case 76: case 91: case 106: case 121: case 136: case 151: case 165: case 178: case 190: case 203: case 216: case 230: case 244: case 257: case 269: case 288:
           character02.position.x = 1;
           break;
-        case 3: case 12: case 23: case 34: case 47:
+        case 3: case 12: case 23: case 34: case 47: case 62: case 77: case 92: case 107: case 122: case 137: case 152: case 166: case 179: case 191: case 204: case 217: case 231: case 245: case 258: case 270: case 281: case 291:
           character02.position.x = 2;
           break;
-        case 4: case 13: case 24: case 35: case 48:
+        case 4: case 13: case 24: case 35: case 48: case 63: case 78: case 93: case 108: case 123: case 138: case 153: case 167: case 180: case 192: case 205: case 218: case 232: case 246: case 259: case 271: case 282:
           character02.position.x = 3;
           break;
-        case 5: case 14: case 25: case 36: case 49:
+        case 5: case 14: case 25: case 36: case 49: case 64: case 79: case 94: case 109: case 124: case 139: case 154: case 168: case 181: case 193: case 206: case 219: case 233: case 247: case 272: case 283: case 289:
           character02.position.x = 4;
           break;
-        case 15: case 26: case 37: case 50:
+        case 15: case 26: case 37: case 50: case 65: case 80: case 95: case 110: case 125: case 140: case 155: case 169: case 194: case 207: case 220: case 234: case 248: case 260: case 290:
           character02.position.x = 5;
           break;
-        case 38: case 51:
+        case 38: case 51: case 66: case 81: case 96: case 111: case 126: case 141: case 156: case 221: case 235: case 261: case 273:
           character02.position.x = 6;
           break;
-        case 52:
+        case 52: case 67: case 82: case 97: case 112: case 127: case 142:
           character02.position.x = 7;
           break;
         default:
           break;
       }
-      this.scene.add(character02);
+      // this.scene.add(character02);
       this.characterArray02.push(character02);
     }
 
@@ -574,6 +632,8 @@ class ThreeApp {
     }, false);
   }
 
+
+
   /**
    * 描画処理
    */
@@ -584,6 +644,8 @@ class ThreeApp {
     // コントロールを更新
     this.controls.update();
 
+
+
     // フラグに応じてオブジェクトの状態を変化させる
     if (this.isDown === true) {
       // Y 軸回転 @@@
@@ -591,18 +653,28 @@ class ThreeApp {
       //   torus.rotation.y += 0.05;
       // });
       // this.scene.remove(character01);
-      this.characterArray01.forEach((character01) => {
-        // character01.rotation.y += 0.05;
-        this.scene.remove(character01);
-      });
-      // this.characterArray02.forEach((character01) => {
-      //   character01.rotation.y += 0.03;
-      // });
-    } else {
-      this.characterArray01.forEach((character01) => {
-        // character01.rotation.y += 0.05;
-        this.scene.add(character01);
-      });
+      // this.scene.add(character02);
+      if (character01_flag) {
+        setTimeout(() => {
+          this.characterArray01.forEach((character01) => {
+            this.scene.remove(character01);
+          });
+          this.characterArray02.forEach((character02) => {
+            this.scene.add(character02);
+          });
+          character01_flag = false;
+        }, 100);
+      } else {
+        setTimeout(() => {
+          this.characterArray02.forEach((character02) => {
+            this.scene.remove(character02);
+          });
+          this.characterArray01.forEach((character01) => {
+            this.scene.add(character01);
+          });
+          character01_flag = true;
+        }, 100);
+      }
     }
 
     // レンダラーで描画
