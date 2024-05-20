2d8a90495da8086d66b0e6ae124606a31aae0c2f
yuta adachi <info@rcwas.com>
2024-05-20 15:47:50 +0900

    5/11課題：ヘルパーを消す。

--- sample_20240511/challenge/script.js
+++ sample_20240511/challenge/script.js
@@ -591,7 +591,7 @@ class ThreeApp {
     // 軸ヘルパー
     const axesBarLength = 5.0;
     this.axesHelper = new THREE.AxesHelper(axesBarLength);
-    this.scene.add(this.axesHelper);
+    // this.scene.add(this.axesHelper);
 
     // コントロール
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
